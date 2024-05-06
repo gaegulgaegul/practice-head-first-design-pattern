cc99dd0980d64dd87c869a1735cfc349bae97044
gaegulgaegul <dla9721@gmail.com>
2024-05-06 15:23:39 +0900

    test MallardDuck#display 테스트 추가

--- src/test/java/io/gaegul/ch01/MallardDuckTest.java
+++ src/test/java/io/gaegul/ch01/MallardDuckTest.java
@@ -5,25 +5,38 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import io.gaegul.util.ConsoleIOTest;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
-@DisplayName("오리 시뮬레이션 게임 - 청둥오리 테스트")
+@DisplayName("오리 시뮬레이션 게임 - 청둥오리는")
 class MallardDuckTest extends ConsoleIOTest {
 
 	@Test
-	void 청둥오리는_꽥꽥_울음소리를_낼_수_있다() {
+	void 꽥꽥_울음소리를_낼_수_있다() {
 		MallardDuck duck = new MallardDuck();
 		duck.quack();
 		assertThat(output()).isEqualTo("꽥꽥~");
 	}
 
 	@Test
-	void 청둥오리는_헤엄을_칠_수_있다() {
+	void 헤엄을_칠_수_있다() {
 		MallardDuck duck = new MallardDuck();
 		boolean result = duck.swim();
 		assertThat(result).isTrue();
+	}
+
+	@Nested
+	class display_메서드를_실행하면 {
+
+		@Test
+		void 청둥오리를_출력한다() {
+			MallardDuck duck = new MallardDuck();
+			duck.display();
+			assertThat(output()).isEqualTo("청둥오리");
+		}
+
 	}
 }
