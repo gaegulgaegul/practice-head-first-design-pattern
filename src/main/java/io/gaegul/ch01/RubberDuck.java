6238e529c1dc2ace811b0f1976a988490503d270
gaegulgaegul <dla9721@gmail.com>
2024-05-06 15:33:53 +0900

    feat RubberDuck 추가

---
+++ src/main/java/io/gaegul/ch01/RubberDuck.java
@@ -0,0 +1,14 @@
+package io.gaegul.ch01;
+
+public class RubberDuck extends Duck {
+
+	@Override
+	public void display() {
+		System.out.println("러버덕");
+	}
+
+	@Override
+	public void quack() {
+		System.out.println("삑삑~");
+	}
+}
