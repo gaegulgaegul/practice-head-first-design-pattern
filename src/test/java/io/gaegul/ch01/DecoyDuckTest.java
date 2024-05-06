8c7063ba54d62e4249b339c3af2e698b4a700dbd
gaegulgaegul <dla9721@gmail.com>
2024-05-06 16:06:32 +0900

    test DecoyDuck 테스트 추가

--- src/test/java/io/gaegul/ch01/DecoyDuckTest.java
+++ src/test/java/io/gaegul/ch01/DecoyDuckTest.java
@@ -3,7 +3,6 @@ package io.gaegul.ch01;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
@@ -13,26 +12,26 @@ import org.junit.jupiter.api.Test;
 import io.gaegul.util.ConsoleIOTest;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
-@DisplayName("오리 시뮬레이션 게임 - 러버덕은")
-class RubberDuckTest extends ConsoleIOTest {
+@DisplayName("오리 시뮬레이션 게임 - 가짜 오리는")
+class DecoyDuckTest extends ConsoleIOTest {
 
 	@Test
-	void 삑삑소리를_낼_수_있다() {
-		Duck duck = new RubberDuck();
-		duck.quack();
-		assertThat(output()).isEqualTo("삑삑~");
+	void 소리를_낼_수_없다() {
+		Duck duck = new DecoyDuck();
+		assertThatThrownBy(duck::quack)
+			.isInstanceOf(UnsupportedOperationException.class);
 	}
 
 	@Test
 	void 헤엄을_칠_수_있다() {
-		Duck duck = new RubberDuck();
+		Duck duck = new DecoyDuck();
 		boolean result = duck.swim();
 		assertThat(result).isTrue();
 	}
 
 	@Test
 	void 날_수_없다() {
-		Duck duck = new RubberDuck();
+		Duck duck = new DecoyDuck();
 		assertThatThrownBy(duck::fly)
 			.isInstanceOf(UnsupportedOperationException.class);
 	}
@@ -41,10 +40,10 @@ class RubberDuckTest extends ConsoleIOTest {
 	class display_메서드를_실행하면 {
 
 		@Test
-		void 러버덕을_출력한다() {
-			Duck duck = new RubberDuck();
+		void 가짜오리를_출력한다() {
+			Duck duck = new DecoyDuck();
 			duck.display();
-			assertThat(output()).isEqualTo("러버덕");
+			assertThat(output()).isEqualTo("가짜 오리");
 		}
 
 	}
