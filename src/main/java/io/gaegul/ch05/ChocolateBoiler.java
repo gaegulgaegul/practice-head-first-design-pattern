2a40bd77eb53973d16fa41d408201318c0244dcb
gaegulgaegul <dla9721@gmail.com>
2024-08-15 16:05:52 +0900

    feat DCL을 적용한 동기화 처리

--- src/main/java/io/gaegul/ch05/ChocolateBoiler.java
+++ src/main/java/io/gaegul/ch05/ChocolateBoiler.java
@@ -7,15 +7,31 @@ public class ChocolateBoiler {
 	private boolean empty;
 	private boolean boiled;
 
-	private static ChocolateBoiler instance = new ChocolateBoiler();
+	/*
+	 * volatile 키워드를 사용하면 멀티스레딩을 쓰더라도
+	 * instance 변수가 Singleton 인스턴스로 초기화되는 과정이 올바르게 진행됩니다.
+	 */
+	private volatile static ChocolateBoiler instance;
 
 	private ChocolateBoiler() {
 		this.empty = true;
 		this.boiled = false;
-		System.out.println("[%s] init ChocolateBoiler".formatted(Thread.currentThread().getName()));
 	}
 
 	public static ChocolateBoiler getInstance() {
+		/* 인스턴스가 있는지 확인하고 없으면 동기화된 블록으로 들어갑니다. */
+		if (instance == null) {
+
+			/* synchronized 키워드를 통해 처음에만 동기화됩니다. */
+			synchronized (Singleton.class) {
+
+				/* 블록에서 다시 한번 변수가 null인지 확인하고 인스턴스를 생성합니다. */
+				if (instance == null) {
+					System.out.println("[%s] init ChocolateBoiler".formatted(Thread.currentThread().getName()));
+					instance = new ChocolateBoiler();
+				}
+			}
+		}
 		System.out.println("[%s] return instance".formatted(Thread.currentThread().getName()));
 		return instance;
 	}
