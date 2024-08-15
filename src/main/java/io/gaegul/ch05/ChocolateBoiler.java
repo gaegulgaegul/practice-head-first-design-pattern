93132bd9a5d706e09756ed8c477515eb58d9a21b
gaegulgaegul <dla9721@gmail.com>
2024-08-15 15:49:09 +0900

    feat synchronized 키워드로 멀티 스레드 문제 해결하기

---
+++ src/main/java/io/gaegul/ch05/ChocolateBoiler.java
@@ -0,0 +1,73 @@
+package io.gaegul.ch05;
+
+/**
+ * 초콜릿 보일러
+ */
+public class ChocolateBoiler {
+	private boolean empty;
+	private boolean boiled;
+
+	private static ChocolateBoiler instance;
+
+	private ChocolateBoiler() {
+		this.empty = true;
+		this.boiled = false;
+	}
+
+	public static synchronized ChocolateBoiler getInstance() {
+		if (instance == null) {
+			System.out.println("[%s] init ChocolateBoiler".formatted(Thread.currentThread().getName()));
+			instance = new ChocolateBoiler();
+		}
+		System.out.println("[%s] return instance".formatted(Thread.currentThread().getName()));
+		return instance;
+	}
+
+	/**
+	 * 보일러에 우유와 초콜릿을 혼합한 재료를 넣습니다.
+	 */
+	public void fill() {
+		/* 보일러가 비어 있을 때만 재표를 넣습니다. */
+		if (isEmpty()) {
+			System.out.println("[%s] empty voiler".formatted(Thread.currentThread().getName()));
+			empty = false;
+			boiled = false;
+		}
+	}
+
+	/**
+	 * 끓인 재료를 다음 단계로 넘깁니다.
+	 */
+	public void drain() {
+		/*
+		 * 보일러가 가득 차 있고 다 끓여진 상태에서만
+		 * 보일러에 들어있는 재표를 다음 단계로 넘깁니다.
+		 */
+		if (!isEmpty() && isBoiled()) {
+			System.out.println("[%s] drain voiler".formatted(Thread.currentThread().getName()));
+			empty = true;
+		}
+	}
+
+	/**
+	 * 재료를 끓입니다.
+	 */
+	public void boil() {
+		/*
+		 * 보일러가 가득 차 있고 아직 끓지 않은 상태에서만
+		 * 초콜릿과 우유가 혼합된 재료를 끓입니다.
+		 */
+		if (!isEmpty() && !isBoiled()) {
+			System.out.println("[%s] boil voiler".formatted(Thread.currentThread().getName()));
+			boiled = true;
+		}
+	}
+
+	public boolean isEmpty() {
+		return empty;
+	}
+
+	public boolean isBoiled() {
+		return boiled;
+	}
+}
---
+++ src/test/java/io/gaegul/ch05/ChocolateBoilerTest.java
@@ -0,0 +1,27 @@
+package io.gaegul.ch05;
+
+import java.util.concurrent.CompletableFuture;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("싱글턴 패턴 테스트")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class ChocolateBoilerTest {
+
+	@Test
+	void 멀티스레딩_테스트() {
+		CompletableFuture<Void> future1 = CompletableFuture.runAsync(ChocolateBoilerTest::executeChocolateBoiler);
+		CompletableFuture<Void> future2 = CompletableFuture.runAsync(ChocolateBoilerTest::executeChocolateBoiler);
+		CompletableFuture.allOf(future1, future2);
+	}
+
+	private static void executeChocolateBoiler() {
+		ChocolateBoiler boiler = ChocolateBoiler.getInstance();
+		boiler.fill();
+		boiler.boil();
+		boiler.drain();
+	}
+}
